a2e9b0803cb353845253609633ad0c585fdd6b73
kotnikd3 <denis.kotnik@gmail.com>
2025-09-23 17:54:59 +0200

    feat: improve home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,69 @@
             </div>
         </div>
     </section>
-    <section>
+
+    <section class="section">
         <h4 class="title is-3" style="color: green;">Stran je v izdelavi, september 2025</h4>
+    </section>
+    
+    <section class="section">
+        <div class="columns is-gapless">
+            <div class="column">
+                <figure>
+                    <img src="@/assets/images/home_1.jpg" alt="psihoanaliza">
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <img src="@/assets/images/home_2.jpg" alt="psihoanaliza">
+                </figure>
+            </div>
+            <div class="column">
+                <figure>
+                    <img src="@/assets/images/home_3.jpg" alt="psihoanaliza">
+                </figure>
+            </div>
+        </div>
+    </section>
+
+    <section class="section">
+        <div class="columns is-gapless">
+            <div class="column">
+                <div class="columns">
+                    <div class="column">
+                        <figure class="is-inline-block" style="width: 50%;">
+                            <a href="https://ecpp.ngo" target="_blank" rel="noopener noreferrer">
+                                <img src="@/assets/images/ecpp_logo.svg" alt="ECPP">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="column">Je strokovno združenje, ki predstavlja slovensko podružnico mednarodnega psihoanalitičnega psihoterapevtskega združenja ECPP.</div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="columns">
+                    <div class="column">
+                        <figure style="width: 50%;">
+                            <a href="https://www.skzp.si/" target="_blank" rel="noopener noreferrer">
+                                <img src="@/assets/images/skzp_logo.png" alt="SKZP">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="column">Je članica Slovenske krovne zveze za psihoterapijo, ki pod svojim okriljem združuje strokovna združenja različnih psihoterapevtskih pristopov v Sloveniji.</div>
+                </div>
+            </div>
+            <div class="column">
+                <div class="columns">
+                    <div class="column">
+                        <figure>
+                            <a href="https://www.europsyche.org/" target="_blank" rel="noopener noreferrer">
+                                <img src="@/assets/images/eap_logo.png" alt="EAP">
+                            </a>
+                        </figure>
+                    </div>
+                    <div class="column">Evropska zveza za psihoterapijo je krovna evropska psihoterapevtska organizacija, katere članici sta tako SKZP kot ECPP in pod okriljem katere posredno deluje tudi EZPPS.</div>
+                </div>
+            </div>
+        </div>
     </section>
 </template>
