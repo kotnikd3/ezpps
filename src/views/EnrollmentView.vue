7d0f605eb96cafd3f0becd05165b2542135b3345
kotnikd3 <denis.kotnik@gmail.com>
2026-01-11 10:55:09 +0100

    feat: redesign Vclnative view

--- src/views/EnrollmentView.vue
+++ src/views/EnrollmentView.vue
@@ -20,88 +20,61 @@
             <div class="block">
                 Društvo ima redne, izredne in častne člane.
             </div>
-        </div>
-    </section>
 
-    <section class="section">
-        <div class="container">
-            <div class="columns is-vcentered">
-                <div class="column">
-                    <div class="notification">
-                        <h5 class="title is-5">Redni član društva</h5>
-                        <div class="block">
-                            Lahko postane vsak polnoleten državljan Republike Slovenije, ki je zaprosil za certifikat ECPP in mu je bil ta s strani Evropske konfederacije psihoanalitičnih psihoterapij (ECPP) podeljen, oziroma vsak imetnik sorodnega certifikata, podeljenega na podlagi izpolnjevanja ustreznih primerljivih kompetenc.
-                        </div>
-                    </div>
+            <div class="notification">
+                <h5 class="title is-5">Redni član društva</h5>
+                <div class="block">
+                    Lahko postane vsak polnoleten državljan Republike Slovenije, ki je zaprosil za certifikat ECPP in mu je bil ta s strani Evropske konfederacije psihoanalitičnih psihoterapij (ECPP) podeljen, oziroma vsak imetnik sorodnega certifikata, podeljenega na podlagi izpolnjevanja ustreznih primerljivih kompetenc.
                 </div>
-                <div class="column">
-                    <div class="notification">
-                        <h5 class="title is-5">Izredni član društva</h5>
-                        <div class="block">
-                            Lahko postane vsak študent/edukant psihoanalitično usmerjenega psihoterapevtskega pristopa oziroma vsak specializant psihoanalitičnega psihoterapevtskega pristopa, ki že dela s pacienti pod supervizijo, a še ni imetnik certifikata ECPP (ali na podlagi usvojenih ustreznih kompetenc temu sorodnega certifikata) in se je pripravljen zavezati pravicam in dolžnostim, ki pripadajo članstvu. Izredni člani so zavezani plačati polovično članarino.
-                        </div>
-                    </div>
+            </div>
+
+            <div class="notification">
+                <h5 class="title is-5">Izredni član društva</h5>
+                <div class="block">
+                    Lahko postane vsak študent/edukant psihoanalitično usmerjenega psihoterapevtskega pristopa oziroma vsak specializant psihoanalitičnega psihoterapevtskega pristopa, ki že dela s pacienti pod supervizijo, a še ni imetnik certifikata ECPP (ali na podlagi usvojenih ustreznih kompetenc temu sorodnega certifikata) in se je pripravljen zavezati pravicam in dolžnostim, ki pripadajo članstvu. Izredni člani so zavezani plačati polovično članarino.
                 </div>
+            </div>
+
+            <h5 class="title is-5">Kdor želi postati redni ali izredni član društva, mora Strokovnemu svetu predložiti:</h5>
+            <div class="content">
+                <ul>
+                    <li><a href="./dokumenti/EZPPS-Pristopna-izjava.pdf" target="_blank" rel="noopener noreferrer">pristopno izjavo</a></li>
+                    <li>izjavo o spoštovanju <a href="./dokumenti/Eticni-kodeks-EZPPS.pdf" target="_blank" rel="noopener noreferrer">Etičnega kodeksa EZPPS (sovpadajočega s kodeksoma ECPP in SKZP)</a></li>
+                    <li>kopijo certifikata ECPP ALI ustrezna dokazila o sorodni strokovni usposobljenosti iz psihoanalitično usmerjenega psihoterapevtskega pristopa (o ustreznosti dokazil in sorodnosti strokovne usposobljenosti odloča Strokovni svet društva) oziroma v primeru kandidature za izredno članstvo potrdilo mentorja o dovoljenju za začetek psihoterapevtskega dela pod supervizijo ALI aktualno kopijo pogodbe o vključenosti v psihoterapevtski študij, če je posameznik študent/edukant psihoanalitično usmerjenega psihoterapevtskega pristopa</li>
+                </ul>
+            </div>
+            <div class="block">
+                V pristopni izjavi kandidat izrazi željo postati član društva in pripravljenost sodelovati pri uresničevanju osnovnih namenov in ciljev društva ter se zaveže, da bo deloval v skladu s Statutom društva in društvenim etičnim kodeksom ter da bo redno plačeval članarino.
+            </div>
+            <div class="block">
+                Vsi obrazci so dostopni na spletni strani društva ali pri predsedniku, podpredsedniku in tajniku društva. Izpolnjena pristopna izjava z vsemi potrebnimi dokazili se pošlje na elektronski naslov društva: <a href="mailto:info@ezpps.si" class="icon-text">info@ezpps.si</a>.
+            </div>
+            
+            <hr>
+
+            <h5 class="title is-5">Odločanje o sprejemu v članstvo</h5>
+            <div class="block">
+                O sprejemu v članstvo odloča Strokovni svet društva na prvem sestanku Strokovnega sveta, ki sledi dnevu prejema pristopne izjave. Član se vpiše v evidenco članstva, ki jo vodita predsednik in tajnik društva.
+            </div>
+            <div class="block">
+                Predsednik društva kandidata pisno obvesti o odločitvi Strokovnega sveta v roku enega tedna po zasedanju Strokovnega sveta. Če je vloga nepopolna, Strokovni svet kandidata o tem obvesti in mu omogoči, da vlogo do naslednjega zasedanja Strokovnega sveta ustrezno dopolni.
+            </div>
+            <div class="block">
+                Če kandidat ne izpolnjuje pogojev, se članstvo zavrne. O zavrnitvi vloge je treba kandidata pisno obvestiti v roku enega tedna od obravnave njegove pristopne izjave z navedbo bistvenih razlogov.
+            </div>
+            <div class="block">
+                Zoper zavrnitev se lahko kandidat pritoži v tridesetih dneh od prejema obvestila o zavrnitvi članstva. Enako pritožbo lahko kandidat vloži, če ne dobi obvestila o članstvu v devetdesetih dneh od svoje poslane pristopne izjave na društvo.
+            </div>
+
+            <hr>
+
+            <h5 class="title is-5">Članarina</h5>
+            <div class="block">
+                Redni in izredni člani društva plačujejo članarino. Članarina se plačuje letno. Višino članarine določi Skupščina. Članarino vplačajo člani najkasneje do konca meseca marca za tekoče leto oziroma ob včlanitvi v društvo za tekoče leto. Način plačevanja članarine določi Strokovni svet društva.
+            </div>
+            <div class="block">
+                Pogoji, našteti na tej strani, so navedeni v <a href="./dokumenti/STATUT-EZPPS-sprejet-na-redni-seji-skupscine-dne-23.10.2020.pdf" target="_blank" rel="noopener noreferrer">Statutu društva</a>.
             </div>
         </div>
     </section>
-
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column">
-                    <h5 class="title is-5">Kdor želi postati redni ali izredni član društva, mora Strokovnemu svetu predložiti:</h5>
-                    <div class="content">
-                        <ul>
-                            <li><a href="./dokumenti/EZPPS-Pristopna-izjava.pdf" target="_blank" rel="noopener noreferrer">pristopno izjavo</a></li>
-                            <li>izjavo o spoštovanju <a href="./dokumenti/Eticni-kodeks-EZPPS.pdf" target="_blank" rel="noopener noreferrer">Etičnega kodeksa EZPPS (sovpadajočega s kodeksoma ECPP in SKZP)</a></li>
-                            <li>kopijo certifikata ECPP ALI ustrezna dokazila o sorodni strokovni usposobljenosti iz psihoanalitično usmerjenega psihoterapevtskega pristopa (o ustreznosti dokazil in sorodnosti strokovne usposobljenosti odloča Strokovni svet društva) oziroma v primeru kandidature za izredno članstvo potrdilo mentorja o dovoljenju za začetek psihoterapevtskega dela pod supervizijo ALI aktualno kopijo pogodbe o vključenosti v psihoterapevtski študij, če je posameznik študent/edukant psihoanalitično usmerjenega psihoterapevtskega pristopa</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="block">
-                        V pristopni izjavi kandidat izrazi željo postati član društva in pripravljenost sodelovati pri uresničevanju osnovnih namenov in ciljev društva ter se zaveže, da bo deloval v skladu s Statutom društva in društvenim etičnim kodeksom ter da bo redno plačeval članarino.
-                    </div>
-                    <div class="block">
-                        Vsi obrazci so dostopni na spletni strani društva ali pri predsedniku, podpredsedniku in tajniku društva. Izpolnjena pristopna izjava z vsemi potrebnimi dokazili se pošlje na elektronski naslov društva: <a href="mailto:info@ezpps.si" class="icon-text">info@ezpps.si</a>.
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section class="section">
-        <div class="container">
-            <div class="columns">
-                <div class="column">
-                    <h5 class="title is-5">Odločanje o sprejemu v članstvo</h5>
-                    <div class="block">
-                        O sprejemu v članstvo odloča Strokovni svet društva na prvem sestanku Strokovnega sveta, ki sledi dnevu prejema pristopne izjave. Član se vpiše v evidenco članstva, ki jo vodita predsednik in tajnik društva.
-                    </div>
-                    <div class="block">
-                        Predsednik društva kandidata pisno obvesti o odločitvi Strokovnega sveta v roku enega tedna po zasedanju Strokovnega sveta. Če je vloga nepopolna, Strokovni svet kandidata o tem obvesti in mu omogoči, da vlogo do naslednjega zasedanja Strokovnega sveta ustrezno dopolni.
-                    </div>
-                    <div class="block">
-                        Če kandidat ne izpolnjuje pogojev, se članstvo zavrne. O zavrnitvi vloge je treba kandidata pisno obvestiti v roku enega tedna od obravnave njegove pristopne izjave z navedbo bistvenih razlogov.
-                    </div>
-                    <div class="block">
-                        Zoper zavrnitev se lahko kandidat pritoži v tridesetih dneh od prejema obvestila o zavrnitvi članstva. Enako pritožbo lahko kandidat vloži, če ne dobi obvestila o članstvu v devetdesetih dneh od svoje poslane pristopne izjave na društvo.
-                    </div>
-                </div>
-                <div class="column">
-                    <div class="block">
-                        <h5 class="title is-5">Članarina</h5>
-                        <div class="block">
-                            Redni in izredni člani društva plačujejo članarino. Članarina se plačuje letno. Višino članarine določi Skupščina. Članarino vplačajo člani najkasneje do konca meseca marca za tekoče leto oziroma ob včlanitvi v društvo za tekoče leto. Način plačevanja članarine določi Strokovni svet društva.
-                        </div>
-                        <div class="block">
-                            Pogoji, našteti na tej strani, so navedeni v <a href="./dokumenti/STATUT-EZPPS-sprejet-na-redni-seji-skupscine-dne-23.10.2020.pdf" target="_blank" rel="noopener noreferrer">Statutu društva</a>.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
 </template>
